7623beb4456c68dc403477f3a86cdba6e31f188d
Vaishanvi Mishra <vaishnavim1105@gmail.com>
2025-09-07 21:34:31 +0530

    Day 5 A2Z

--- Arrays/med/MaxSubarray.java
+++ Arrays/med/MaxSubarray.java
@@ -5,31 +5,22 @@ public class MaxSubarray {
         int[] nums={-2,1,-3,4,-1,2,1,-5,4};
         int[] nums2={5,4,-1,7,8};
 
-        int ans= maxSubarray(nums);
-        // int ans2=kadaneAlgo(nums);
+        int ans=kadaneAlgo(nums);
+        int ans2=kadaneAlgo(nums2);
+
         
         System.out.println(ans);
-        // System.out.println(ans2);
+        System.out.println(ans2);
     }
 
-    // BRUTE FORCE
-    static int maxSubarray(int[]  nums){
-        int i=0;
-        int sum=nums[0];
-        int psum=sum;
-        int max=Integer.MIN_VALUE;
-        for(int j=1;j<nums.length;j++){
-            sum+=nums[j];
-            if(psum>sum){
-                sum-=nums[i];
-                i++;
-            }
-            max=Math.max(sum,max);
+    // KANDANE ALGO
+    static int kadaneAlgo(int[] nums){
+        int max=nums[0];
+        int csum=nums[0];
+        for(int i=1;i<nums.length;i++){
+            csum=Math.max(nums[i],csum+nums[i]);
+            max=Math.max(max,csum);
         }
         return max;
     }
-
-    // static int kadaneAlgo(int[] nums){
-
-    // }
 }
